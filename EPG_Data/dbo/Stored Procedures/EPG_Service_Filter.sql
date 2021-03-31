f657af18218b39a4003c4c20c32c8d3d4eda1720
Top coder <topcoder1208@gmail.com>
2021-03-31 14:37:24 +0300

    3.31

---
+++ EPG_Data/dbo/Stored Procedures/EPG_Service_Filter.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[EPG_Service_Filter]
+	@Date DATE,
+	@FilterText varchar(100)
+AS 
+BEGIN
+SELECT [Id]
+          ,[Sid_ServiceName]
+          ,[Sid_VM_ServiceId]
+          ,[Sid_Epg_ServiceId]
+          ,[Sid_ServiceLogo]
+          ,[Sid_ChannelResolution]
+          ,[Sid_VM_ServiceGroup]
+          ,[Sid_RowChanges]
+    FROM [dbo].[ServiceInformationData]
+	where Sid_Epg_ServiceId in (SELECT distinct Psd_ServiceIdReference FROM DBO.ProgramScheduleData WHERE CONVERT(DATE, Psd_ScheduledStartTime) = @Date and Psd_ProgramCrid in (
+				select distinct Pid_Crid from DBo.ProgramInformationData where Pid_TitleMain like concat('%', @FilterText, '%')))
+END
+GO
